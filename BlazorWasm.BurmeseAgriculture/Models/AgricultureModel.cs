f22d57b3ed423a0b5237261822465c223f033865
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2024-01-26 00:18:27 +0630

    [Complete] Get List

---
+++ BlazorWasm.BurmeseAgriculture/Models/AgricultureModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWasm.BurmeseAgriculture.Models
+{
+    public record AgricultureModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ BlazorWasm.BurmeseAgriculture/Pages/Home.razor.cs
@@ -0,0 +1,38 @@
+﻿using BlazorWasm.BurmeseAgriculture.Models;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorWasm.BurmeseAgriculture.Pages
+{
+    public partial class Home
+    {
+        private List<AgricultureModel> DataList = new List<AgricultureModel>();
+
+        private async Task LoadJavaScript()
+        {
+            await Task.Delay(1000);
+            await JsRuntime.InvokeVoidAsync("loadJs", new string[]
+            {
+                "assets/js/main.js"
+            }.ToList());
+            await List();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await LoadJavaScript();
+            }
+        }
+
+        private async Task List()
+        {
+            var response = await httpClient.GetFromJsonAsync<List<AgricultureModel>>("data/BurmeseAgriculture.json");
+            DataList = response!;
+            Console.WriteLine(JsonConvert.SerializeObject(DataList, Formatting.Indented));
+        }
+    }
+}
